5cff88febf39b9840a87b096938ed27d4c76732c
Karina <karinasokolova252@gmail.com>
2025-12-15 15:10:39 +0200

    Remove unnecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "rooms/index"
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations',
