26a0a8fa7d12e356590cf9ffb8e82652228a15b5
Karina <karinasokolova252@gmail.com>
2025-12-18 14:49:24 +0200

    Fix controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,10 @@
 
 class ApplicationController < ActionController::Base
   after_action :set_user_cookie, if: :user_signed_in?
-  layout :resolve_layout
 
   private
 
   def set_user_cookie
     cookies.encrypted[:user_id] = current_user.id
-  end
-
-  def resolve_layout
-    turbo_frame_request? ? false : "application"
   end
 end
