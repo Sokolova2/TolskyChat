282300401aee277bf10783f6b634cff0ffe43b77
Karina <karinasokolova252@gmail.com>
2025-12-22 18:16:52 +0200

    Fix valisates

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,6 +3,4 @@ class Conversation < Room
   has_many :messages, foreign_key: :conversation_id, dependent: :destroy
 
   accepts_nested_attributes_for :participants
-
-  validates
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,4 @@
 class Room < ApplicationRecord
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, presence: true
   scope :public_rooms, -> { where(is_private: false) }
-
-  validates :name, presence: true
-
 end
