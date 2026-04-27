cb58e524b8e85fdf5664e6484d45246b19e9d4c3
Sokolova2 <karinasokolova252@gmail.com>
2026-04-27 18:47:26 +0300

    Add method for create personal chat

--- app/services/room_service.rb
+++ app/services/room_service.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class RoomService
-  def initialize(params, user)
+  def initialize(params = {}, user)
     @params = params
     @user = user
   end
@@ -17,11 +17,13 @@ class RoomService
   end
 
   def call_chat(second_user)
-    chat = PersonalChat.create(@params)
+    chat = PersonalChat.create(name: second_user.login, is_private: true)
 
     if chat.save
-      chat.participants.create(user: @user, room: chat)
-      chat.participants.create(user: second_user, room: chat)
+      chat.participants.create(user: @user, room: chat, role: 'Member')
+      chat.participants.create(user: second_user, room: chat, role: 'Member')
     end
+
+    chat
   end
 end
