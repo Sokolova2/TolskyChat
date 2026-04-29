e7b4e2ddf7d450f59a14a39544dd51bcbe3889d4
Sokolova2 <karinasokolova252@gmail.com>
2026-04-29 17:00:54 +0300

    add production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'example.com' }
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV.fetch('USER_MAIL', nil),
+    password: ENV.fetch('MAIL_PASSWORD', nil),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   config.i18n.fallbacks = true
 
   config.active_record.dump_schema_after_migration = false
